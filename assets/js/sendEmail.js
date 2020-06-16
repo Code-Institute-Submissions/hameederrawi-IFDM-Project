c42450edebb1250f95d39a341001a2e69fc07b0f
hameederrawi <hameed@medsuk.co.uk>
2020-06-16 02:16:53 +0000

    added contact form fields to sendEmail.js

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "company_name": contactForm.cname.value,
+        "tel_number": contactForm.tel.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return alert("Message sent, Thank you");
+}
