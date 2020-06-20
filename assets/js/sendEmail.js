157b1e6ab4c955000f6e57be8cd91044951f00ae
hameederrawi <hameed@medsuk.co.uk>
2020-06-20 16:56:37 +0000

    added form validators for Fname, Lname, Message, also added bad language filter for Message to mask the bad word with starts, bad words example for testing (bad1, bad2, bad3) the function name is Abuse

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,24 +1,75 @@
 function ValidateEmail(emailaddress)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(emailaddress.value.match(mailformat))
-{
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-emailaddress.focus();
-return false;
-}
-}
+    {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(emailaddress.value.match(mailformat))
+    {
+    return true;
+    }
+    else
+    {
+    alert("You have entered an invalid email address!");
+    emailaddress.focus();
+    return false;
+    }
+    }
+
+function ValidateFname()
+    { 
+    var letters = /^[A-Za-z]+$/;
+    if(fname.value.match(letters))
+    {
+    return true;
+    }
+    else
+    {
+    alert('Please input alphabet characters only in the First Name Field');
+    return false;
+    }
+    }
+
+var Abuse = function ()
+        {
+            var com = document.getElementById('inforequest');
+            var filterWords = ["bad1", "bad2", "bad3"];
+            // "i" is to ignore case and "g" for global
+            var rgx = new RegExp("("+filterWords.join("|")+")", "gi");
+            com.value = com.value.replace(rgx, "****");
+
+            // 'return true;' when you will be sure that all your bad words are caught to continue to the next function otherwise a return false will stop the next step.
+            return true;
+        }
+
+function ValidateLname()
+    { 
+    var letters = /^[A-Za-z]+$/;
+    if(lname.value.match(letters))
+    {    
+    return true;
+    }
+    else
+    {
+    alert('Please input alphabet characters only in the Last Name Field');
+    return false;
+    }
+    }
+
+// If the length of the element's string is 0 then display helper message 
+function ValidateMessage() 
+   {
+     if (inforequest.value.length <= 15)
+      { 
+         alert("Message field is too short!");  	
+         return false; 
+      }  	
+      return true; 
+    } 
 
 function sendMail(contactForm) {
     emailjs.send("gmail", "realitycontact", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "from_fname": contactForm.fname.value,
+        "from_lname": contactForm.lname.value,
         "company_name": contactForm.cname.value,
-        "tel_number": contactForm.tel.value,
+        "from_email": contactForm.emailaddress.value,            
         "info_request": contactForm.inforequest.value
     })
     
@@ -31,7 +82,8 @@ function sendMail(contactForm) {
         }
     );
     
-    return alert("Message sent, Thank you");
+    alert("Message sent, Thank you");
+    return false;
     
 }
 
